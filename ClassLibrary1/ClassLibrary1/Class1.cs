41be489edb65b352b2b19f67dbe08bb564be4079
Kostya05A <danilenko.ka2005@gmail.com>
2023-03-30 09:03:44 +0700

    frist upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClassLibrary
+{
+    public class Geometry
+    {
+        public double Compute(double r, double h, double volt)
+        {
+            throw new NotImplementedException();
+        }
+
+        struct Cylinder
+        {
+            public double H { get; set; }
+            public double R { get; set; }
+            public Cylinder(double h, double r)
+            {
+                H = h;
+                R = r;
+            }
+            public double Volt() => 2 * Math.PI * R * H;
+            public double Sas() => 2 * Math.PI * R * (R + H);
+            public override string ToString() => $"H = {H}; R = {R}; S = {Sas().ToString("0.###")}; V={Volt().ToString("0.###")}";
+        }   
+    }
+}
---
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MathClassLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int R = 6;
+            int H = 4;
+            // Оидаемый результат - должен совпасть с рациональным значением
+            double Volt = 150.79645;
+            double Sas = 376.99112;
+            
+            Geometry g = new Geometry();
+            double actual1 = g.Compute(R, H, Volt);
+            double actual2 = g.Compute(R, H, Sas);// Получение фактического результата
+
+            Assert.AreEqual(Volt, actual1);
+            Assert.AreEqual(Sas, actual2);
+            // Итог - тест не будет пройден ввиду наличия погрешности результатов 
+        }
+    }
+}
